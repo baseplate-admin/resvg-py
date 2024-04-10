8e3dff48b15b57e60ae505da19b3730ec14a47e0
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-04-10 12:02:16 +0600

    Update lib.rs

--- src/rust/lib.rs
+++ src/rust/lib.rs
@@ -1,7 +1,48 @@
+/*
+Based on
+* https://github.com/RazrFalcon/resvg/blob/master/crates/resvg/src/main.rs
+* https://github.com/mrdotb/resvg_nif/blob/master/native/resvg/src/lib.rs
+*/
+
 use base64::{engine::general_purpose, Engine as _};
 use pyo3::prelude::*;
 use resvg;
 use resvg::usvg;
+
+struct Opts {
+    font_family: Option<String>,
+    font_size: u32,
+    serif_family: Option<String>,
+    sans_serif_family: Option<String>,
+    cursive_family: Option<String>,
+    fantasy_family: Option<String>,
+    monospace_family: Option<String>,
+    font_files: Vec<String>,
+    font_dirs: Vec<String>,
+    skip_system_fonts: bool,
+}
+
+fn load_font(options: &mut Opts, fontdb: &mut usvg::fontdb::Database) {
+    for path in &options.font_files {
+        if let Err(e) = fontdb.load_font_file(path) {
+            println!("Failed to load '{}' cause {}.", path.to_string(), e);
+        }
+    }
+
+    for path in &options.font_dirs {
+        fontdb.load_fonts_dir(path);
+    }
+
+    let take_or =
+        |family: Option<String>, fallback: &str| family.unwrap_or_else(|| fallback.to_string());
+
+    fontdb.set_serif_family(take_or(options.serif_family.take(), "Times New Roman"));
+    fontdb.set_sans_serif_family(take_or(options.sans_serif_family.take(), "Arial"));
+    fontdb.set_cursive_family(take_or(options.cursive_family.take(), "Comic Sans MS"));
+    fontdb.set_fantasy_family(take_or(options.fantasy_family.take(), "Impact"));
+    fontdb.set_monospace_family(take_or(options.monospace_family.take(), "Courier New"));
+}
+
 fn render_svg(tree: &usvg::Tree) -> Result<tiny_skia::Pixmap, String> {
     let mut pixmap = tiny_skia::Pixmap::new(
         tree.size().to_int_size().width(),
@@ -16,7 +57,7 @@ fn render_svg(tree: &usvg::Tree) -> Result<tiny_skia::Pixmap, String> {
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
-fn sum_as_string(py: Python, svg_string: String) -> PyResult<String> {
+fn sum_as_string(svg_string: String) -> PyResult<String> {
     let xml_tree = {
         let xml_opt = usvg::roxmltree::ParsingOptions {
             allow_dtd: true,
