86239defd2ffce5fa1420e9c3ae50096c91b5560
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-04-10 15:20:43 +0600

    Fuck this (#4)
    * Update lib.rs
    
    * add
    
    * add
    
    * add
    
    * Update test.py
    
    * add
    
    * add
    
    * Update lib.rs

--- src/python/test.py
+++ src/python/test.py
@@ -1,10 +1,5 @@
-from resvg_py import sum_as_string
+from resvg_py import svg_to_base64
 
-svg = """
-<svg width="300" height="130" xmlns="http://www.w3.org/2000/svg">
-  <rect width="200" height="100" x="10" y="10" rx="20" ry="20" fill="blue" />
-</svg>
-"""
-lt = sum_as_string(svg)
-
+svg = ""
+lt = svg_to_base64(svg_string=svg)
 print(lt)
