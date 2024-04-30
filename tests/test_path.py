0f3a2be6c7b93e7fda588e3a8e9e4d5055246600
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-04-30 11:27:39 +0600

    Update test_path.py

--- tests/test_path.py
+++ tests/test_path.py
@@ -7,10 +7,10 @@ svg_output = "iVBORw0KGgoAAAANSUhEUgAAAGwAAABfCAYAAAANiCLOAAAWd0lEQVR4Ae3gAZAkSZ
 
 
 def test_path():
-    base = resvg_py.svg_to_base64(str(Path(BASE_DIR, "acid.svg")))
+    base = resvg_py.svg_to_base64(str(Path(BASE_DIR, "acid.svg").resolve()))
     assert base == svg_output
 
 
 def test_gzip_path():
-    base = resvg_py.svg_to_base64(str(Path(BASE_DIR, "acid.svg.gz")))
+    base = resvg_py.svg_to_base64(str(Path(BASE_DIR, "acid.svg.gz").resolve()))
     assert base == svg_output
