098441bb81c71f7d91dcf6d71ac5f72e7595d9a3
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-05-08 21:43:39 +0600

    a

--- resvg_py.pyi
+++ resvg_py.pyi
@@ -27,6 +27,28 @@ def svg_to_bytes(
     ],
     background: str | None = None,
 ) -> list[bytes]:
-    """"""
+    """
+    :param svg_str: A string containing valid svg.
+    :param svg_path: A path to a valid svg.
+    :param width: An Integer containing the pixels size for width.
+    :param height: An Integer containing the pixels size for height.
+    :param zoom: An Integer containing the zoom percentage.
+    :param dpi: An Integer containing DPI size for the svg rendering.
+    :param resources_dir: A directory that contains resources for svg rendering. Such as `foreign objects <https://developer.mozilla.org/en-US/docs/Web/SVG/Element/foreignObject>`_.
+    :param languages: A list of string containing the languages used for `svg` rendering
+    :param font_size: An integer describing the font_size.
+    :param font_family: A string that describes the font family used in SVG.
+    :param serif_family: A string that describes the serif font family used in SVG.
+    :param sans_serif_family: A string that describes the sans serif font family used in SVG.
+    :param cursive_family: A string that describes the cursive font family used in SVG.
+    :param fantasy_family: A string that describes the fantasy font family used in SVG.
+    :param monospace_family: A string that describes the monospace font family used in SVG.
+    :param font_files: A list of paths that contain the font file.
+    :param font_dirs: A list of directories that contain the font file. This parameter will add all the present files in the directory.
+    :param shape_rendering: The `shape rendering method <https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/shape-rendering>`_ used in resvg. **Defaults to "geometric_precision"**.
+    :param text_rendering: The `text rendering method <https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/text-rendering>`_ used in resvg. **Defaults to "optimize_legibility"**.
+    :param image_rendering: The `image rendering method <https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/image-rendering>`_ used in resvg. **Defaults to "optimize_quality"**.
+    :param background: A `CSS color <https://developer.mozilla.org/en-US/docs/Web/CSS/color_value>`_ value that describes the canvas size.
+    """
 
     ...
