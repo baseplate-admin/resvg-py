5e674e7fccc5b67658f2f39a46f4e38d28b3b5c9
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2026-05-19 11:03:34 +0600

    Update test_font_render.py
    Delete tests/layer/test_multiple_layer_svg.py
    
    Delete tests/layer/test.svg
    
    Delete tests/path directory

--- tests/font/test_font_render.py
+++ tests/font/test_font_render.py
@@ -67,6 +67,10 @@ def test_font_directory():
 def test_font_cache_consistency():
     path = os.path.join(BASE_DIR, "ink.svg")
     font = str(Path(BASE_DIR, "font_dir"))
-    first = resvg_py.svg_to_bytes(svg_path=path, skip_system_fonts=True, font_dirs=[font])
-    second = resvg_py.svg_to_bytes(svg_path=path, skip_system_fonts=True, font_dirs=[font])
+    first = resvg_py.svg_to_bytes(
+        svg_path=path, skip_system_fonts=True, font_dirs=[font]
+    )
+    second = resvg_py.svg_to_bytes(
+        svg_path=path, skip_system_fonts=True, font_dirs=[font]
+    )
     assert first == second
